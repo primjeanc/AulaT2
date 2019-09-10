60deffb16112eafe28e9087d3c88e704596df9e6
Jean Prim <gangrelrox@hotmail.com>
2019-09-10 17:22:36 -0300

    Commit

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Texto informativo para consultar o livro pelo numero de registro do sistema
+            Console.WriteLine("Informe o livro a ser consultado:");
+            /*Parte do codigo que recebe as informações de registro do livro */
+            var numeroDoLivro = Console.ReadLine();
+            //Aqui realizo a comparação das informações do livro informado com o disponível em estoque
+            if (numeroDoLivro == "123456")
+            {
+                //Informo que este livro já está locado
+                Console.WriteLine("Livro Indisponível");
+                Console.ReadKey();
+                //Finaliza o método
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Deseja locar o livro? (1)sim (2)não");
+                var resposta = Console.ReadLine();
+                if (resposta == "1")
+                {
+                    Console.WriteLine("Livro locado");
+                    Console.ReadKey();
+                    return;
+                }
+            }
+            
+                        
+            
+        }
+    }
+}
