d0788ce18f9c170d5373ee763ce3775eac416b93
Jean Prim <gangrelrox@hotmail.com>
2019-09-18 14:15:39 -0300

    1809-menu classes e metodos

--- IniciandoLista/BaseApp/Program.cs
+++ IniciandoLista/BaseApp/Program.cs
@@ -14,6 +14,9 @@ namespace BaseApp
             MenuClasses();//versao classe
 
         }
+        /// <summary>
+        /// Menu de opcoes utlizando classes
+        /// </summary>
         private static void MenuClasses()//menu usando biblioteca de classes
         {
             var escolha = string.Empty;
@@ -21,31 +24,62 @@ namespace BaseApp
             Console.WriteLine("Menu de Opções disponíveis:");
             Console.WriteLine("[1]Árvore");
             Console.WriteLine("[2]Área de Quadrado");
-            Console.WriteLine("[3]Lista de Cervejas");
-            Console.WriteLine("[4]Marcas de Carros mais procurados:");
-            Console.WriteLine("[5]Sair:");
+            Console.WriteLine("[3]Listas Cadastradas");
+            //Console.WriteLine("[4]Marcas de Carros mais procurados:");
+            Console.WriteLine("[4]Sair:");
             escolha = Console.ReadKey().KeyChar.ToString();
 
             switch (escolha)
             {
                 case "1":
-                    { MyFuckingTree.StupidTree(); } 
+                    { MyFuckingTree.StupidTree(); MenuClasses(); } //arvore
                     break;
                 case "2":
-                    { CalculoArea(); MenuClasses(); }
+                    { CalculoArea(); MenuClasses(); }//calc area quadrado
                     break;
                 case "3":
-                    { Cervejas.ClasseListaCervejas(); }                    
+                    { Console.Clear(); ListaMarcas(); }    //abre sub menu de listas cerveja/carro                
                     break;
-                case "4":
+                /*case "4":
                     { MontadorasCarros.ClasseListaCarros(); }
-                    break;
-                case "5":
+                    break;*/
+                case "4":
                     {
                         Console.WriteLine("Saindo...");
                         return;
-                    }                    
+                    }
                 default:
+                    { Console.Clear(); MenuClasses(); }
+                    break;
+            }
+
+        }
+        private static void ListaMarcas()
+        {
+            var escolha = string.Empty;
+            Console.WriteLine("*#*#*#*#*#*#*#*#*#*#**#*#*#*#*");
+            Console.WriteLine("Menu de Opções disponíveis:");
+            Console.WriteLine("[1]Listar Cervejas cadastradas");
+            Console.WriteLine("[2]Listar Marcas de carro cadastradas");            
+            Console.WriteLine("[3]Retonar ao menu inicial:");
+            escolha = Console.ReadKey().KeyChar.ToString();
+
+            switch (escolha)
+            {
+                case "1":
+                    { ListaDeMarcas.ClasseListaCervejas(); ListaMarcas(); }
+                    break;
+                case "2":
+                    { ListaDeMarcas.ClasseListaCarros(); ListaMarcas(); }
+                    break;
+                case "3":
+                    {
+                        Console.WriteLine("Retornar ao menu inicial");
+                        MenuClasses();
+                        break;
+                    }
+                default:
+                    { Console.Clear(); ListaMarcas(); }
                     break;
             }
 
--- IniciandoLista/MinhaBiblicoteca/ListaDeMarcas.cs
+++ IniciandoLista/MinhaBiblicoteca/ListaDeMarcas.cs
@@ -6,8 +6,23 @@ using System.Threading.Tasks;
 
 namespace MinhaBiblicoteca
 {
-    public class MontadorasCarros
+    public class ListaDeMarcas
     {
+        public static void ClasseListaCervejas()
+        {
+            Console.Clear();
+            var minhaLista = new List<string>();
+
+            minhaLista.Add("Budweiser");
+            minhaLista.Add("Corona");
+            minhaLista.Add("Stella Artois");
+            minhaLista.Add("Franziskaner");
+            minhaLista.Add("Original");
+
+            minhaLista.ForEach(i => Console.WriteLine(i)); //foreach simplificado para lista/coleções            
+            Console.WriteLine("Aperte qualquer tecla para continuar");
+            Console.ReadKey();
+        }
         public static void ClasseListaCarros()
         {
             Console.Clear();
