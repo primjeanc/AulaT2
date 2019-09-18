a4960cdc892e56ddb2c3bd959d51803d5f8c9db9
Jean Prim <gangrelrox@hotmail.com>
2019-09-18 17:27:43 -0300

    1809-17h27

---
+++ MinhaPrimeiraClass/MySecondClass/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySecondClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fruitList = new List<Fruits>
+            {
+                new Fruits()
+                {
+                    Name = "Morango",
+                    Quantity = 5 },
+                new Fruits()
+                {
+                    Name = "Banana",
+                    Quantity = 7 },
+                new Fruits()
+                {
+                    Name = "Pera",
+                    Quantity = 9 },
+
+            };
+            fruitList.ForEach(i => Console.WriteLine($"Nome: {i.Name} -- Quantidade: {i.Quantity}"));
+            Console.ReadKey();
+        }
+    }
+}
