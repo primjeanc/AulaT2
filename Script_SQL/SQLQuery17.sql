cbe020b76387780063e22426fdf830d89156b3b7
Jean Prim <gangrelrox@hotmail.com>
2019-10-01 08:33:34 -0300

    Reorganizando GIT por semana (locacao de livros esta no dia 20)

---
+++ Script_SQL/SQLQuery17.sql
@@ -0,0 +1,25 @@
+﻿create table Carros(
+Id int not null primary key identity(1,1),
+Modelo nchar(50),
+Marca nchar(50) not null,
+Placa nchar(7),
+Ano int,
+Ativo bit default 1,
+UsuarioCriacao int,
+UsuarioAlteracao int,
+DataCriacao datetime default getdate(),
+DataAlteracao datetime default getdate())
+
+create table Locacoes(
+Id int not null primary key identity(1,1),
+UsuarioId int not null,
+CarroId int not null,
+Valor money,
+DataInicial datetime default getdate(),
+DataDevolucao datetime,
+Ativo bit default 1,
+UsuarioCriacao int,
+UsuarioAlteracao int,
+DataCriacao datetime default getdate(),
+DataAlteracao datetime default dateadd(day,5,getdate())
+)
---
+++ Script_SQL/SQLQuery18.sql
@@ -0,0 +1,3 @@
+﻿insert into Locacoes (UsuarioId,CarroId,Valor) values (0,0,500)
+go
+select *from Locacoes
