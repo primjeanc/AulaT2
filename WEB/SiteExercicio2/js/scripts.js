f2a759cd1920df199358f6a51d069ad8e38cdba3
Jean Prim <gangrelrox@hotmail.com>
2019-10-16 17:25:56 -0300

    web convert currency

--- WEB/SiteExercicio2/js/scripts.js
+++ WEB/SiteExercicio2/js/scripts.js
@@ -1,5 +1,13 @@
 function bvindas() {
-    var cvalor = document.getElementById("converter").value;
-    alert('Convertendo '+cvalor+' reais:')
-    alert('Convertido para dolar: '+cvalor/4);
+    var cvalor = document.getElementById("reais").value;
+    alert('Convertendo para Dolar Comercial a 4.05:' + '-+-+-+- Total em dolar: ' + cvalor / 4.05)
+    
+}
+function convertendo() {
+    var fvalor = document.getElementById("reais").value / 4.00;  
+}
+function convertREAL() {
+    var cvalor = (parseFloat(document.getElementById("reais").value / 4.05)).toFixed(2);
+    $('input[id="money"]').val(cvalor);
+
 }
