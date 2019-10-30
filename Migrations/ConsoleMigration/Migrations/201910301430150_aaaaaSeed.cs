bc783b7db8be4a840d771a471b495c66b96558ff
Jean Prim <gangrelrox@hotmail.com>
2019-10-30 11:58:42 -0300

    seed

---
+++ Migrations/ConsoleMigration/Migrations/201910301430150_aaaaaSeed.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleMigration.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class aaaaaSeed : DbMigration
+    {
+        public override void Up()
+        {
+            DropColumn("dbo.Usuarios", "Ativo");
+            DropColumn("dbo.Usuarios", "UsuarioCriacao");
+            DropColumn("dbo.Usuarios", "UsuarioAlteracao");
+            DropColumn("dbo.Usuarios", "DataCriacao");
+            DropColumn("dbo.Usuarios", "DataAlteracao");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Usuarios", "DataAlteracao", c => c.DateTime(nullable: false));
+            AddColumn("dbo.Usuarios", "DataCriacao", c => c.DateTime(nullable: false));
+            AddColumn("dbo.Usuarios", "UsuarioAlteracao", c => c.Int(nullable: false));
+            AddColumn("dbo.Usuarios", "UsuarioCriacao", c => c.Int(nullable: false));
+            AddColumn("dbo.Usuarios", "Ativo", c => c.Boolean(nullable: false));
+        }
+    }
+}
---
+++ Migrations/ConsoleMigration/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleMigration.Model;
+
+namespace ConsoleMigration
+{
+    
+    class Program
+    {
+        static MigrationContext db = new MigrationContext();
+        static void Main(string[] args)
+        {
+            db.Usuarios.ToList().ForEach(x=>Console.WriteLine(x.Nome));//inicia base de dados
+            Console.ReadKey();
+        }
+    }
+}
