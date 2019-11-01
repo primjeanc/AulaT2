9da0010945906b9225547d956f445642de0ff5c6
Jean Prim <gangrelrox@hotmail.com>
2019-11-01 17:29:27 -0300

    commit MEME

---
+++ NETCOREwebAPI/NETCOREwebAPI/Models/FirstCodeContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NETCOREwebAPI.Models
+{
+    public class FirstCodeContext:DbContext
+
+    {
+        public FirstCodeContext(DbContextOptions<FirstCodeContext> options)
+            : base(options)
+        { 
+        }
+        public DbSet<Carro> Carros
+
+
+    }
+}
